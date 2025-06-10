1fcdc4ed2c7a0e02d60849473c9bd0c862fd97bd
3nadhk <3nadh.g2@gmail.com>
2025-06-10 23:00:29 +0530

    latest

--- 03condition.sh
+++ 03condition.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 NUMBER=$1
 
-if [ NUMBER -eq 10 ]
-then
-	echo "Given number $NUMBER is equal to 10!"
-fi
-exit 0
-
 	if [ $NUMBER -lt 10 ]
 then
 	echo " Given number $NUMBER is less than 10"
 else
 	echo " Given number $NUMBER is not less than 10"
 	fi
-
-if [ NUMBER -eq 10 ]
-then
-	echo "Given number $NUMBER is equal to 10!"
-fi
