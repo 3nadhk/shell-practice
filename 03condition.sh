bafa35661489c409f4ab657ea7a4ed3aa73497ae
3nadhk <3nadh.g2@gmail.com>
2025-06-10 19:30:47 +0530

    ifcondition with eq test

--- 03condition.sh
+++ 03condition.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 NUMBER=$1
 
+if [ NUMBER -eq 10 ]
+then
+	echo "Given number $NUMBER is equal to 10!"
+fi
+exit 0
+
 	if [ $NUMBER -lt 10 ]
 then
 	echo " Given number $NUMBER is less than 10"
