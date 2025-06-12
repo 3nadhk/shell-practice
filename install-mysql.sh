5d853a7bcf02e97bce5df32f0174b2e5b98dc10a
3nadhk <3nadh.g2@gmail.com>
2025-06-12 22:33:20 +0530

    test update

--- install-mysql.sh
+++ install-mysql.sh
@@ -6,12 +6,26 @@ then
     exit 1 #Give other than 0 up to 127
 else    
     echo "You are running with root access"
-fi
-dnf install mysqldddd -y
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
+then
+    echo "MY SQL is not installed -- going to install"
+    if [ $? -eq 0 ]
 then 
     echo "Installing MYSQL is ... SUCCESS"
 else
     echo "Installing MYSQL is ... FAILED"
     exit 1
 fi   
+
+else 
+    echo "MySQL is already installed.. Nothing to do"
+    exit 1    
+fi
+dnf install mysqldddd -y
+# if [ $? -eq 0 ]
+# then 
+#     echo "Installing MYSQL is ... SUCCESS"
+# else
+#     echo "Installing MYSQL is ... FAILED"
+#     exit 1
+# fi   
