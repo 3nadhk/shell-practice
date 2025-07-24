662062e28ad3e15aa4f459d3e56ee89cbf249fff
3nadhk <3nadh.g2@gmail.com>
2025-07-24 21:15:40 +0530

    hellow world updated

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,4 +1,9 @@
-echo "Test1 : How are you"
-echo "test2 : I am good"
-echo "tech1 :: all going good sofar"
-echo "This is updated with vscode from local machine"
+
+#!/bin/bash
+#$1="Test1"
+# $2="Test2"
+
+echo "$1 : How are you"
+echo "$2 : I am good"
+echo "$1 :: all going good sofar"
+echo "$1 , $2 This is updated with vscode from local machine"
---
+++ 10-backup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
